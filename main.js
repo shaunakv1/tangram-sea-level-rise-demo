9ea118022e482036302dfc707bc4c8b4b76c4bc8
shaunakv1 <shaunakv1@gmail.com>
2016-06-06 10:27:00 -0400

    added noaa seal level rise layers from 0 to 6 ft and added up and down functions to switch sea level rise

--- main.js
+++ main.js
@@ -28,8 +28,17 @@ map = (function () {
         scene: 'scene.yaml',
         attribution: '<a href="https://mapzen.com/tangram" target="_blank">Tangram</a> | &copy; OSM contributors | <a href="https://mapzen.com/" target="_blank">Mapzen</a>'
     });
-
-    var rasterLayer = L.tileLayer('http://d.tile.stamen.com/terrain-background/{z}/{x}/{y}.jpg');
+    
+    var currentSlr = 4;
+    var slrLayers = { 
+                        slr_0ft: L.tileLayer('https://www.coast.noaa.gov/arcgis/rest/services/dc_slr/slr_0ft/MapServer/tile/{z}/{y}/{x}'),
+                        slr_1ft: L.tileLayer('https://www.coast.noaa.gov/arcgis/rest/services/dc_slr/slr_1ft/MapServer/tile/{z}/{y}/{x}'),
+                        slr_2ft: L.tileLayer('https://www.coast.noaa.gov/arcgis/rest/services/dc_slr/slr_2ft/MapServer/tile/{z}/{y}/{x}'),
+                        slr_3ft: L.tileLayer('https://www.coast.noaa.gov/arcgis/rest/services/dc_slr/slr_3ft/MapServer/tile/{z}/{y}/{x}'),
+                        slr_4ft: L.tileLayer('https://www.coast.noaa.gov/arcgis/rest/services/dc_slr/slr_4ft/MapServer/tile/{z}/{y}/{x}'),
+                        slr_5ft: L.tileLayer('https://www.coast.noaa.gov/arcgis/rest/services/dc_slr/slr_5ft/MapServer/tile/{z}/{y}/{x}'),
+                        slr_6ft: L.tileLayer('https://www.coast.noaa.gov/arcgis/rest/services/dc_slr/slr_6ft/MapServer/tile/{z}/{y}/{x}')
+                     };
 
     window.layer = layer;
     var scene = layer.scene;
@@ -46,10 +55,27 @@ map = (function () {
         // Scene initialized
         layer.on('init', function() {
         });
-        rasterLayer.addTo(map);
+        slrLayers["slr_"+currentSlr+"ft"].addTo(map);
         layer.addTo(map);
 
     });
+
+    window.slrUp = function() {
+        if(currentSlr + 1 <= 6){
+            map.removeLayer(slrLayers["slr_"+currentSlr+"ft"]);
+            currentSlr += 1;
+            slrLayers["slr_"+currentSlr+"ft"].addTo(map);
+        }
+    }
+
+    window.slrDown = function() {
+        if(currentSlr - 1 >=0 ){
+            map.removeLayer(slrLayers["slr_"+currentSlr+"ft"]);
+            currentSlr -= 1;
+            slrLayers["slr_"+currentSlr+"ft"].addTo(map);
+        }
+        
+    }
 
     return map;
 
